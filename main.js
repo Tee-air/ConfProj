78c18fea4998b6946d119e290a257fac4399e5d3
Tiago Rimbaud <tiago_rimbaud@hotmail.com>
2020-01-24 14:54:26 +0100

    reduction des nodes modules

--- main.js
+++ main.js
@@ -22,61 +22,68 @@ app.use(express.static(__dirname + '/lib/bootstrap'));
 app.use(express.static(__dirname + '/lib'));
 app.use(express.static(__dirname + '/node_modules'));
 
-app.use(bodyParser.json());       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+    extended: true
 }));
 
 console.log('Server ON');
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     console.log('yes');
-    res.sendFile('editTemplate.html', { root: path.join(__dirname, '/lib/vues/') });
-    
+    res.sendFile('editTemplate.html', {
+        root: path.join(__dirname, '/lib/vues/')
+    });
+
 });
 
-app.get('/Profile', function(req, res){
+app.get('/Profile', function (req, res) {
     var keyAPI = "RGAPI-1fb24620-e295-49cb-8213-637cbe74fcf4"
-    res.sendFile('DisplayProfile.html', { root: path.join(__dirname, '/lib/vues/') });
+    res.sendFile('DisplayProfile.html', {
+        root: path.join(__dirname, '/lib/vues/')
+    });
     //findya.ajaxReq('https://euw1.api.riotgames.com/tft/summoner/v1/summoners/by-name/XxBipBipxX?api_key='+keyAPI, '');
 });
 
-app.get('/editVue', function(req, res) {
+app.get('/editVue', function (req, res) {
     //res.sendFile('editWithPackery.html', { root: path.join(__dirname, '/lib/vues/') });
-    res.sendFile('vueEditor.html', { root: path.join(__dirname, '/lib/vues/') });
+    res.sendFile('vueEditor.html', {
+        root: path.join(__dirname, '/lib/vues/')
+    });
     //new Vue("", 1, "VueTest");
 });
 
-app.get('/connectionBDD', function(req, res){
+app.get('/connectionBDD', function (req, res) {
     var server = "DESKTOP-J384JJ1";
     var user = "TeeAir";
     var bdd = "ProjetBI";
-   MSSQLConnect();
-    
-    
+    MSSQLConnect();
+
+
 });
 
-app.post('/addBlock', function(req, res){
+app.post('/addBlock', function (req, res) {
     console.log(req.body);
 
-    console.log("requested");
-    return "yes";
+    
+    return res.send("yes");
 });
 
 
-app.post('/saveAll', function(req, res){
+app.post('/saveAll', function (req, res) {
     console.log(req.body);
 
     var db = new PouchDB('https://localhost:5984/config');
-    db.get('vue_1').then(function (doc){
-        //doc.blocks = req.body;
+    db.get('vue_1').then(function (doc) {
+        return res.send(doc);
     });
+
+    return res.send("yes*n");
 });
 
-function getBlocks(){
+function getBlocks() {
 
 }
 
 
 app.listen(8081);
-
