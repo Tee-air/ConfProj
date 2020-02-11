ed5f5188d8c0883b5b0e0d72640888e62ba8163b
Rimbaud Tiago <tiago.rimbaud@horoquartz.fr>
2020-02-11 16:24:09 +0100

    Connection file for API

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//Quel Modules sont vraiment utilisé et important.
+//Quel Modules sont vraiment utilisé et important ??
 
 var http = require('http');
 var url = require('url');
@@ -7,13 +7,14 @@ var querystring = require('querystring');
 var fs = require('fs');
 var express = require('express');
 var path = require('path');
-var got = require('got');
 var fs = require('fs');
 //var packery = require('packery');
 var bodyParser = require('body-parser');
 var managerVue = require('./lib/ManagePackery/manageVue.js');
+var managerData = require('./lib/ManagePackery/manageData.js');
 
-
+var managerCon = require('./lib/ManagePackery/manageCon.js');
+var PouchDB = require('pouchdb');
 
 var app = express();
 // TODO : Réduire au minimum les fichiers transiter vers le client
@@ -27,6 +28,9 @@ app.use(bodyParser.json()); // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
     extended: true
 }));
+
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 console.log('Server ON');
 
@@ -48,20 +52,22 @@ app.get('/Profile', function (req, res) {
 
 //app.get('/editVue', function (req, res) {
 
-    //var managerV = new managerVue();
-    //var  doc = managerV.initVue(1);
-    
-    //res.sendFile('vueEditor.html', {
-        //root: path.join(__dirname, '/lib/vues/')
-    //});
-    //res.render("contentDoc",{content: doc.content});
+//var managerV = new managerVue();
+//var  doc = managerV.initVue(1);
+
+//res.sendFile('vueEditor.html', {
+//root: path.join(__dirname, '/lib/vues/')
 //});
+//res.render("contentDoc",{content: doc.content});
+//});
+
+
 
 app.use('/editVue', (req, res, next) => {
     var managerV = new managerVue();
-    var  doc = managerV.initVue(1);
-    
-    fs.readFile('./lib/vues/vueEditor.html', 'utf8', function(err, data){
+    var doc = managerV.initVue(1);
+
+    fs.readFile('./lib/vues/vueEditor.html', 'utf8', function (err, data) {
         console.log(data);
         console.log(data.getElementById("toolMenu"));
     });
@@ -71,8 +77,8 @@ app.use('/editVue', (req, res, next) => {
     });
 
     console.log(doc);
-    res.status(200).json({content: doc});
-  });
+    res.status(200).json({ content: doc });
+});
 
 app.get('/connectionBDD', function (req, res) {
     var server = "DESKTOP-J384JJ1";
@@ -84,7 +90,7 @@ app.get('/connectionBDD', function (req, res) {
 app.post('/addBlock', function (req, res) {
     console.log(req.body);
 
-    
+
     return res.send("yes");
 });
 
@@ -95,6 +101,25 @@ app.post('/saveAll', function (req, res) {
     managerV.updateBlocks(req.body);
     return res.send("yes*n");
 });
+
+app.get('/showData', async function (req, res) {
+    let managerD = new managerData();
+    let doc = await managerD.showDoc(1);
+
+    console.log(doc);
+
+    res.status(200).json(doc.content);
+});
+
+app.get('/showDataAPI', function (req, res) {
+    let managerC = new managerCon("API");
+    managerC.keyAPI = "RGAPI-9425b2f6-d413-44d0-9705-c17c674e9914";
+    let data = managerC.getCon();
+
+    res.status(200).json(data);
+});
+
+
 
 
 app.listen(8081);
